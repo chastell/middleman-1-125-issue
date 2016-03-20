d77b971f87e0aac9b2eba38cfa4000435e1c0b7b
Piotr Szotkowski <chastell@chastell.net>
2016-03-20 12:44:00 +0100

    Middleman 3 example

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'middleman',      '~> 3.4'
+gem 'middleman-blog', '~> 3.5'
---
+++ config.rb
@@ -0,0 +1,7 @@
+activate :blog do |blog|
+  blog.name      = '1/125'
+  blog.permalink = '{title}.html'
+  blog.prefix    = '1/125'
+end
+
+activate :directory_indexes
